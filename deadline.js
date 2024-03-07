b15dce62c7aa04a5800c52c2bcd3a425b58c988b
fariq <149790664+nuxxttt@users.noreply.github.com>
2024-03-07 20:23:45 +0700

    Update deadline.js

--- deadline.js
+++ deadline.js
@@ -1,3 +1,3 @@
 var sbmptn_current_time, sbmptn_deadline_time;
 sbmptn_current_time = new Date();
-sbmptn_deadline_time = '23 Jun 2022 15:00:00 +0700';
+sbmptn_deadline_time = '13 Jun 2024 15:00:00 +0700';
